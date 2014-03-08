f24e1a3ef7536a24e9faf470a82f61fc90df838c
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2014-03-08 19:20:37 +0100

    Support for minimizing main window to Windows tray icon

--- src/DesktopTimer/Views/MainView.xaml.cs
+++ src/DesktopTimer/Views/MainView.xaml.cs
@@ -6,12 +6,13 @@
 
 namespace DesktopTimer.Views
 {
+    using System;
     using System.Windows;
 
     /// <summary>
     /// Interaction logic for <c>MainView.xaml</c>.
     /// </summary>
-    public partial class MainView : Window
+    public partial class MainView
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="MainView"/> class.
@@ -19,6 +20,44 @@ namespace DesktopTimer.Views
         public MainView()
         {
             this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Called when when the application's notify icon is double clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void OnNotifyIconDoubleClick(object sender, RoutedEventArgs e)
+        {
+            this.Show();
+            this.WindowState = WindowState.Normal;
+        }
+
+        /// <summary>
+        /// Called when the main window is loaded.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+
+        /// <summary>
+        /// Called when the main window's state changes.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void OnStateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this.Hide();
+            }
+            else
+            {
+                this.Show();
+            }
         }
     }
 }
